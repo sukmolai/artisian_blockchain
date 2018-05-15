6135ad7a7c596e80717e4e6dfe0af0fc63c8e75e
Your Name <linsong1996@yahoo.ca>
2018-05-14 22:21:35 -0700

    added score

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import datetime
+
 print("Hello. Welcome to Artisian Blockchain")
 print("Press enter to start")
 
 # Typing game
+start_time = datetime.datetime.now()
 
+text = input()
+
+end_time = datetime.datetime.now()
+score = end_time - start_time
+print (score)
 
 print("Thanks for playing")
 quit()
