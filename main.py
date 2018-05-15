1a727b600dbfe187e558619b34db1e2985294d0f
AustT1996 <tripp.aust@gmail.com>
2018-05-14 22:35:26 -0700

    Added a sentence getting function

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import datetime
+import random
+
+def random_sentence():
+    with open("sentence_bank.txt") as f:
+        lines = f.readlines()
+    return random.choice(lines)
 
 print("Hello. Welcome to Artisian Blockchain")
 print("\n\n")
 print("Press enter to continue. Type q to quit.")
 quit = input().lower().strip() == "q"
 
+print(random_sentence())
 
 # Typing game
 start_time = datetime.datetime.now()
