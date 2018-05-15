840a986fa7253c1b420ed3ec4c4726a8b4f3186c
Your Name <linsong1996@yahoo.ca>
2018-05-14 22:44:15 -0700

    WE OUTTIE

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import random
 def random_sentence():
     with open("sentence_bank.txt") as f:
         lines = f.readlines()
-    return random.choice(lines)
+    return random.choice(lines).strip()
 
 print("Hello. Welcome to Artisian Blockchain")
 print("\n\n")
 print("Press enter to continue. Type q to quit.")
 quit = input().lower().strip() == "q"
 
-print(random_sentence())
+gen_sentence = random_sentence()
+print(gen_sentence)
 
 # Typing game
 start_time = datetime.datetime.now()
 
 text = input()
+if text == gen_sentence:
+    print("Good job fam")
+else:
+    print("U dun goofed")
 
 end_time = datetime.datetime.now()
 score = end_time - start_time
